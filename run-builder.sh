fd9d3a042cfb75743912d051cde2a3a17f5c3b8b
Martin Vool <martin.vool@cloud.ee>
2019-04-10 14:55:50 +0300

    Add login to registry

--- run-builder.sh
+++ run-builder.sh
@@ -18,6 +18,9 @@ BUILD_TIME=${BUILD_TIME:-23:00:00}
 AUTO_REBUILD_HOUR=${AUTO_REBUILD_HOUR:-6}
 BUILDER_TYPE=${BUILDER_TYPE:-dev}
 
+
+ docker login -u $DOCKER_USER -p $DOCKER_AUTH $ORG
+
 # param: message text content
 function post_slack_message {
     MSG='{"username":"OTP data builder '$BUILDER_TYPE'","text":"'$1'"}'
